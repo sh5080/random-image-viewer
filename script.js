b0ec987d84e9933c2e2fc17706b6bdc92ff94590
김승환 <prpn97@gmail.com>
2023-07-15 17:20:48 +0900

    fix: public 경로 동적으로 수정

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const randomizeButton = document.getElementById("randomize-button");
 const randomImage = document.getElementById("random-image");
-const imageDir = "public/";
+const imageDir = "./public/";
 let images = []; // 이미지 파일 이름들을 저장할 배열
 
 // 이미지를 랜덤으로 보여주는 함수
@@ -27,8 +27,8 @@ async function fetchImages() {
     for (let i = 0; i < fileList.length; i++) {
       const fileName = fileList[i].getAttribute("href");
       if (fileName.endsWith(".png")) {
-        // images.push(fileName); // 로컬 테스트
-        images.push(imageDir + encodeURIComponent(fileName));
+        images.push(fileName); // 로컬 테스트
+        // images.push(imageDir + encodeURIComponent(fileName));
       }
     }
 
