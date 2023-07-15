5ffbf79848177235a6d7dc4e4f20ac3c0a26da15
김승환 <prpn97@gmail.com>
2023-07-15 22:41:52 +0900

    Fix: github api 사용하는 방식으로 변경

--- script.js
+++ script.js
@@ -1,49 +1,40 @@
+const randomizeButton = document.getElementById("randomize-button");
 const randomImage = document.getElementById("random-image");
-// const imageDir = "https://sh5080.github.io/randomImageViewer/public/";
-const imageDir = "./public";
-// 이미지를 랜덤으로 보여주는 함수
-async function getRandomImage() {
-  // async 키워드 추가
-  const images = await fetchImages(); // await 키워드 추가
-  if (images.length > 0) {
-    const randomIndex = Math.floor(Math.random() * images.length);
-    const imageName = images[randomIndex];
-    randomImage.src = imageName; // 이미지의 URL을 그대로 설정
-  }
-}
+const repoOwner = "sh5080";
+const repoName = "randomImageViewer";
+const imageDir = "public";
 
-// 이미지 파일들을 불러오는 함수
+// GitHub 레포지토리에서 이미지 파일 이름 목록을 가져오는 함수
 async function fetchImages() {
   try {
-    const response = await fetch(imageDir);
+    const response = await fetch(
+      `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${imageDir}`
+    );
     if (!response.ok) {
-      throw new Error("Network response was not ok");
+      throw new Error("네트워크 응답이 실패했습니다");
     }
-
-    const data = await response.text();
-    const fileList = new DOMParser()
-      .parseFromString(data, "text/html")
-      .getElementsByTagName("a");
-
-    const images = [];
-    for (let i = 0; i < fileList.length; i++) {
-      const fileName = fileList[i].getAttribute("href");
-      if (fileName.endsWith(".png")) {
-        images.push(fileName); // 이미지 파일의 절대 경로를 배열에 추가
-      }
-    }
-
-    return images;
+    const data = await response.json();
+    const imageFiles = data.filter(
+      (file) => file.type === "file" && file.name.endsWith(".png")
+    );
+    return imageFiles.map((file) => file.download_url);
   } catch (error) {
-    console.error("Error fetching images:", error);
+    console.error("이미지 가져오기 오류:", error);
     return [];
   }
 }
 
-// 페이지 로드시 랜덤 이미지를 보여줌
-getRandomImage();
+// 리스트에서 랜덤한 이미지를 화면에 표시하는 함수
+async function getRandomImage() {
+  const images = await fetchImages();
+  if (images.length > 0) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    randomImage.src = images[randomIndex];
+  }
+}
 
-// 랜덤 버튼 클릭시 이미지 변경
-document
-  .getElementById("randomize-button")
-  .addEventListener("click", getRandomImage);
+// "다음 문제" 버튼에 이벤트 리스너를 추가합니다.
+randomizeButton.addEventListener("click", getRandomImage);
+
+// 페이지 로드시 랜덤한 이미지를 보여줍니다.
+getRandomImage();
