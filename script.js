3253ea86860acd203952aa94ea61caf5e4a3e21b
김승환 <prpn97@gmail.com>
2023-07-15 16:43:53 +0900

    fix: fetch 방식으로 변경

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
-const randomizeButton = document.getElementById('randomize-button');
-const randomImage = document.getElementById('random-image');
-const imageDir = 'public/';
+const randomizeButton = document.getElementById("randomize-button");
+const randomImage = document.getElementById("random-image");
+const imageDir = "public/";
 let images = []; // 이미지 파일 이름들을 저장할 배열
 
-// XMLHttpRequest를 사용하여 이미지 파일을 불러옴
-function fetchImages() {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', imageDir);
-  xhr.responseType = 'document';
-
-  xhr.onload = function () {
-    const fileList = xhr.response.getElementsByTagName('a');
-    for (let i = 0; i < fileList.length; i++) {
-      const fileName = fileList[i].getAttribute('href');
-      if (fileName.endsWith('.png')) {
-        images.push(fileName);
-      }
-    }
-    getRandomImage(); // 이미지를 모두 불러왔으면 랜덤 이미지를 보여줌
-  };
-
-  xhr.send();
-}
-
+// 이미지를 랜덤으로 보여주는 함수
 function getRandomImage() {
   if (images.length > 0) {
     const randomIndex = Math.floor(Math.random() * images.length);
-    randomImage.src = images[randomIndex];
+    const imageName = images[randomIndex];
+    randomImage.src = imageName;
   }
 }
 
-randomizeButton.addEventListener('click', getRandomImage);
-fetchImages(); // 이미지 파일들을 불러옴
+// 이미지 파일들을 불러오는 함수
+async function fetchImages() {
+  try {
+    const response = await fetch(imageDir);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.text();
+    const fileList = new DOMParser()
+      .parseFromString(data, "text/html")
+      .getElementsByTagName("a");
+    for (let i = 0; i < fileList.length; i++) {
+      const fileName = fileList[i].getAttribute("href");
+      if (fileName.endsWith(".png")) {
+        images.push(fileName);
+      }
+    }
+
+    // 모든 이미지를 로드하고 랜덤 이미지를 보여줌
+    getRandomImage();
+  } catch (error) {
+    console.error("Error fetching images:", error);
+  }
+}
+
+randomizeButton.addEventListener("click", getRandomImage); // 랜덤 버튼 클릭시 이미지 변경
+fetchImages(); // 페이지 로드시 이미지 파일들을 불러옴
