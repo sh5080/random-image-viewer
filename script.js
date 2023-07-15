ed6a6e6ee195986e6fbbe31052dd0475d630d4fb
김승환 <prpn97@gmail.com>
2023-07-15 21:51:37 +0900

    test

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const randomizeButton = document.getElementById("randomize-button");
 const randomImage = document.getElementById("random-image");
-const imageDir = "./public";
-let images = ["a.png"]; // 이미지 파일 이름들을 저장할 배열
+const imageDir = "./public/";
 
 // 이미지를 랜덤으로 보여주는 함수
 function getRandomImage() {
-  if (images.length > 0) {
-    const randomIndex = Math.floor(Math.random() * images.length);
-    const imageName = images[randomIndex];
-    randomImage.src = imageName; // 이미지의 URL을 그대로 설정
-  }
+  fetchImages().then((images) => {
+    if (images.length > 0) {
+      const randomIndex = Math.floor(Math.random() * images.length);
+      const imageName = images[randomIndex];
+      randomImage.src = imageDir + imageName; // 이미지의 URL을 그대로 설정
+    }
+  });
 }
 
 // 이미지 파일들을 불러오는 함수
@@ -24,19 +24,26 @@ async function fetchImages() {
     const fileList = new DOMParser()
       .parseFromString(data, "text/html")
       .getElementsByTagName("a");
+
+    const images = [];
     for (let i = 0; i < fileList.length; i++) {
       const fileName = fileList[i].getAttribute("href");
       if (fileName.endsWith(".png")) {
-        images.push(imageDir + fileName + ".png"); // 로컬 테스트
+        images.push(fileName); // 이미지 파일 이름을 배열에 추가
       }
     }
 
-    // 모든 이미지를 로드하고 랜덤 이미지를 보여줌
-    getRandomImage();
+    return images;
   } catch (error) {
     console.error("Error fetching images:", error);
+    return [];
   }
 }
 
-randomizeButton.addEventListener("click", getRandomImage); // 랜덤 버튼 클릭시 이미지 변경
-fetchImages(); // 페이지 로드시 이미지 파일들을 불러옴
+// 페이지 로드시 랜덤 이미지를 보여줌
+getRandomImage();
+
+// 랜덤 버튼 클릭시 이미지 변경
+document
+  .getElementById("randomize-button")
+  .addEventListener("click", getRandomImage);
